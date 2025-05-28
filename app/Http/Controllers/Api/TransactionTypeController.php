b480b4530b4c821965d8652ff4348cb64f427993
tarekbassiouny <tarek.bassiuony22@gmail.com>
2025-05-28 04:41:15 +0300

    some fixes

--- app/Http/Controllers/Api/TransactionTypeController.php
+++ app/Http/Controllers/Api/TransactionTypeController.php
@@ -6,27 +6,29 @@ use App\Interfaces\TransactionTypeServiceInterface;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TransactionTypeResource;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Http\Resources\ErrorResource;
 
 class TransactionTypeController extends Controller
 {
     public function __construct(private TransactionTypeServiceInterface $service) {}
 
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         return TransactionTypeResource::collection($this->service->getAllTypes());
     }
 
-    public function subtype($id)
+    public function subtype($id): AnonymousResourceCollection|JsonResponse
     {
         try {
             $subtypes = $this->service->getSubtypesByType($id);
+            
             return TransactionTypeResource::collection($subtypes);
         } catch (\Exception $e) {
             Log::error($e);
-            return response()->json([
-                'status' => 'error',
-                'message' => __('type.invalid_type'),
-            ], 500);
+            return (new ErrorResource(['message' => __('type.invalid_type')]))
+                ->response()->setStatusCode(500);
         }
     }
 }
